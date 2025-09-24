7cd13f39d67ebe8396c3a73bbcbbfd884c069db7
Vijaya <2020bcs021@sggs.ac.in>
2025-09-25 00:04:02 +0530

    second commit

---
+++ react-frontend/build/precache-manifest.e553b52ab316beadf8260044939343ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66502e879003807ca547091c59217a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4226474189caeb4df9e1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3d693a8507aa1767a2bb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4226474189caeb4df9e1",
+    "url": "/static/js/2.e6473232.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6473232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d693a8507aa1767a2bb",
+    "url": "/static/js/main.dc1fab25.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
